af5c719755b6905ffa83e9ed2d7ddb3597364ef0
Alaa Badran <info@alaabadran.com>
2013-10-09 15:24:25 +0300

    Working fine now.

--- alert.js
+++ alert.js
@@ -11,11 +11,11 @@ $(function (){
         }
         $('#jsalert_modal h4').html(title);
         $('#jsalert_modal .modal-body').html(msg);
-        $('#jsalert_modal').fadeIn(400);
+        $('#jsalert_modal').modal('show');
     };
     // preparing HTML layout for new alert()
     // Adding Modal HTML.
-    var html = '<div class="modal" role="dialog" id="jsalert_modal"><div class="modal-dialog" aria-hidden="true"><div class="modal-content">'+
+    var html = '<div class="modal fade" role="dialog" id="jsalert_modal" aria-hidden="true"><div class="modal-dialog"><div class="modal-content">'+
         '<div class="modal-header">'+
           '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>'+
           '<h4 class="modal-title">{{TITLE}}</h4>'+
@@ -25,12 +25,10 @@ $(function (){
         '</div>'+
     '</div></div></div>';
     var alertObj = $('body').append(html);
+    $('#jsalert_modal').modal('hide');
     // Showing your message
     $('#jsalert').on('click', function (e){
         e.preventDefault();
-        alert($('#alert_message').val()); 
-    });
-    $(document).on('click', '#jsalert_modal .close', function (e){
-        $('#jsalert_modal').fadeOut(300);
+        alert($('#alert_message').val(), $('#alert_title').val()); 
     });
 });
