e0a03837dc16722d10d26646bbf8b9bace652896
Alaa Badran <info@alaabadran.com>
2013-10-10 09:53:38 +0300

    Fixes and added comments to the script

--- jsalert.js
+++ jsalert.js
@@ -4,23 +4,43 @@ var $alert = window.alert;
 
 $.jsalert = {
 
+    /**
+     * Used to not initialize more than once.
+     */
     initialized: false,
 
+    /**
+     * Initialize the script.
+     */
     _init: function (){
         if(alert.options.bootstrap == true){
             this._bootstrapTemplate();
         } else {
             this._template();
         }
+        // Setting the title.
         alert.options.title = $(document).prop('title');
     },
 
+    /**
+     * Preparing alert HTML template
+     */
     _template: function (){},
 
-    _alert: function (msg, title){},
+    /**
+     * Show alert message.
+     */
+    _alert: function (msg){},
 
+    /**
+     * Add events to elements.
+     */
     _events: function (){},
 
+
+    /**
+     * Preapre the HTML layout for alert from Twitter Bootstrap 3
+     */
     _bootstrapTemplate: function (){
         var html = '<div class="modal fade" role="dialog" id="jsalert_modal" aria-hidden="true"><div class="modal-dialog"><div class="modal-content">'+
             '<div class="modal-header">'+
@@ -36,6 +56,9 @@ $.jsalert = {
         $('#jsalert_modal').modal('hide');
     },
 
+    /**
+     * Show alert message for Bootstrap 3
+     */
     _bsAlert: function (msg, title){
         if(msg == ''){
             msg = '<i>Empty</i>';
@@ -45,6 +68,10 @@ $.jsalert = {
         $('#jsalert_modal').modal('show');
     },
 
+    /**
+     * Fires the message.
+     * This method specifies what function to call.
+     */
     fire: function (msg){
         //console.log(msg);
         if(alert.options.bootstrap === true){
@@ -59,23 +86,21 @@ $.jsalert = {
     }
 };
 
+// Re-Defining alert()
 var alert = function (msg){
     //console.log($.alert._bootstrap_template);
     $.jsalert.fire(msg);
 };
+
+// Setting options
 alert.options = {
-    'bootstrap': true,
-    'html_tags': false  
-};
-alert.defaults = {
     'bootstrap': true,
     'html_tags': false
 };
 
 $(function (){
-    //alert('message');
     $('#jsalert').on('click', function (e){
         e.preventDefault();
-        alert($('#alert_message').val()); 
+        alert($('#alert_message').val());
     });
 });
